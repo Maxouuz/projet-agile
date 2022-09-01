c1d4a8fff3ed4b9993f2c4ba7f113f2b17c70e43
Maxence Stievenard <maxence.stievenard.etu@univ-lille.fr>
2022-09-01 16:20:57 +0200

    lane.java

---
+++ ZombIUT/src/ZombIUT/Lane.java
@@ -0,0 +1,20 @@
+package ZombIUT;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Lane {
+	
+	final int SIZE = 10;
+	ArrayList stuff = new ArrayList();
+	
+	public Lane() {
+		int i = 0;
+		while(i < SIZE) {
+			Random rand = new Random();
+			
+			i++;
+		}
+	}
+	
+}
