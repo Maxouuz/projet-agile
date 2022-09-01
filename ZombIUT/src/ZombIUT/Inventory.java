d92fb91df53ab9ed1c731ca4be164a9b2d843b4f
Charles Top-pottez <charles.toppottez.etu@univ-lille.fr>
2022-09-01 16:32:00 +0200

    creation inventaire et ressources

---
+++ ZombIUT/src/ZombIUT/Inventory.java
@@ -0,0 +1,13 @@
+package ZombIUT;
+
+import java.util.HashMap;
+
+public class Inventory {
+	HashMap<Ressources, Integer> inventory;
+	
+	public Inventory() {
+		inventory = new HashMap();
+	}
+
+	
+}
---
+++ ZombIUT/src/ZombIUT/Ressources.java
@@ -0,0 +1,12 @@
+package ZombIUT;
+
+public enum Ressources {
+
+	EAU("de l'eau");
+	
+	private String nom;
+	
+	Ressources(String nom) {
+		this.nom = nom;	
+	}
+}
