cf1522d75a07003ebd01d3bef31df6873dc29c05
Charles Top-pottez <charles.toppottez.etu@univ-lille.fr>
2022-09-02 10:47:21 +0200

    evenements fait reste à tester

--- ZombIUT/src/ZombIUT/Events.java
+++ ZombIUT/src/ZombIUT/Events.java
@@ -4,23 +4,34 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class Events {
-	
+
 	private String separator = File.separator;
 	private String path = System.getenv() + separator + "ZombiIUT" + separator + "ressources" + separator + "Event.csv";
-	
+	private List<List<String>> lines = new ArrayList();
 	
 	public void collecteDonner () {
-		try (BufferedReader fichier = new BufferedReader(new FileReader(path))) {
-			String line = fichier.readLine();
-			while (line != null) {  
-				
-			}  
-		} catch (IOException e) {  
-			e.printStackTrace();  
+		String delimiter = ",";
+		String line;
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			while((line = br.readLine()) != null){
+				List<String> values = Arrays.asList(line.split(delimiter));
+				lines.add(values);
+			}
+		} catch (IOException e){
+			e.printStackTrace();
 		}
-		
 	}
-
+	
+	public String dropEvent() {
+		collecteDonner();
+		return lines.get( new Random().nextInt(3)).toString();
+	}
+	
+	
 }
