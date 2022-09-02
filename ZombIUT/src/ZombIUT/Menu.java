704effe80937818de13718bcfa83335bd8b05a62
Raymond Khampan <raymond.khampan.etu@univ-lille.fr>
2022-09-02 09:13:57 +0200

    reglage

--- ZombIUT/src/ZombIUT/Menu.java
+++ ZombIUT/src/ZombIUT/Menu.java
@@ -6,58 +6,60 @@ import java.util.Scanner;
 public class Menu {
 	//Main
 	static Scanner sc;
-	
+
 	public Menu() {
 		sc = new Scanner(System.in);
 	}
-	
+
 	public static void main(String[] args) {
 		Menu m = new Menu();
 		m.menu();
 
 	}
+
+
 	public static int menu() {
 		int x;
 		boolean ff = true;
-		
+
 		while(ff) {
 			x=0;
 			MenuDuDebut();
-			
+
 			System.out.println("Choix:");
-			
+
 
 			while(x!=1 && x!=2 && x!=3) {    
-			  x = sc.nextInt();
+				x = sc.nextInt();
 			} 
-			
+
 			if(x==1) {
-				
-				
+
+
 				System.out.println();
-				System.out.println("Pseudo 1:");
+				System.out.println("Pseudo :");
 				String name = sc.next();
 				Player p = new Player(name);
 				System.out.println(p);
-				
-				
+				// mettre le jeu
+
 			} else if (x==2) {
 				regle();
 				System.out.println("Entrer une touche pour revenir");
 				x = sc.nextInt();
-				
+
 
 			} else if (x==3){
-				
-			}else if (x==4){
-				
-			}	else if (x==5){
+
+			} else if (x==4){
+
+			} else if (x==5){
 				ff=false;
 			}		
 		}
-				return 42;
+		return 42;
 	}
-	
+
 	public static void MenuDuDebut() {
 		System.out.println("Bienvenue dans ZombIUT");
 		System.out.println("Choix :");
@@ -67,12 +69,12 @@ public class Menu {
 		System.out.println("4: Score");
 		System.out.println("5: Exit");
 	}
-	
-	
+
+
 	public static void regle() {
 		System.out.println("oui");
 
 	}
-	
-	
+
+
 }
