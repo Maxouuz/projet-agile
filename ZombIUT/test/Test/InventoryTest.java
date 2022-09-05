2538cf49876f20fe17fc1d1cd2f2eb8862619586
Simon Bocquet <simon.bocquet.etu@univ-lille.fr>
2022-09-05 09:25:15 +0200

    Inventory test

--- ZombIUT/test/Test/InventoryTest.java
+++ ZombIUT/test/Test/InventoryTest.java
@@ -1,8 +1,9 @@
 package Test;
 
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,7 +20,7 @@ public class InventoryTest {
 	}
 	
 	@Test
-	private void addTest() {
+	public void addTest() {
 		assertTrue(i.getInventory().isEmpty());
 		i.add(Ressources.EAU, 2);
 		assertFalse(i.getInventory().isEmpty());
@@ -29,4 +30,33 @@ public class InventoryTest {
 		assertTrue(i.add(Ressources.KIT, 1));
 		assertFalse(i.add(Ressources.PAIN, 1));
 	}
+	
+	@Test
+	public void sameObjectTest() {
+		i.add(Ressources.EAU, 1);
+		assertTrue(i.sameObject(Ressources.EAU));
+		assertFalse(i.sameObject(Ressources.CARTE));
+	}
+	
+	@Test
+	public void countObjectsTest() {
+		i.add(Ressources.CARTE, 1);
+		i.add(Ressources.FUSIL, 1);
+		i.add(Ressources.HACHE, 1);
+		i.add(Ressources.KIT, 1);
+		assertEquals(4, i.countObjects());
+	}
+	
+	@Test
+	public void clearTest() {
+		i.add(Ressources.CARTE, 1);
+		i.add(Ressources.FUSIL, 1);
+		i.add(Ressources.HACHE, 1);
+		i.add(Ressources.KIT, 1);
+		assertEquals(4, i.countObjects());
+		i.clearAll();
+		assertEquals(0, i.countObjects());
+	}
+	
+	
 }
